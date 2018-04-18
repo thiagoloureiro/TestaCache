7258f5a22a04133f49e27fea4d0f4f0777a3efd4
thiagoloureiro <thiagoguaru@me.com>
2018-04-18 17:48:06 +0200

    Removed Old Project

--- CacheApi.NetCore/Model/Customer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CacheApi.Model
-{
-    public class Customer
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
